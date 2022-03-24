3342bd2057d2feebe52322c33b97c072315b5cc3
huang shukui <hsk139231357@users.noreply.github.com>
2022-03-24 18:32:27 +0800

    Update gridsome.config.js
    修改配置文件

--- gridsome.config.js
+++ gridsome.config.js
@@ -17,7 +17,7 @@ module.exports = {
     {
       use: '@gridsome/source-strapi',
       options: {
-        apiURL: 'http://localhost:1337',
+//         apiURL: 'http://localhost:1337',
         queryLimit: 1000, // Defaults to 100
         contentTypes: ['post', 'tag'],
         typeName: 'Strapi',
